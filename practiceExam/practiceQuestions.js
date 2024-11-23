800f1dd41bd87f125d4a5566cdad283179202e0c
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-11-23 17:31:03 +0900

    added question for practice exam

--- practiceExam/practiceQuestions.js
+++ practiceExam/practiceQuestions.js
@@ -1,8 +1,10 @@
 export const sculptureList = [{
-  name: 'Homenaje a la Neurocirugía',
-  artist: 'Marta Colvin Andrade',
-  description: 'Although Colvin is predominantly known for abstract themes that allude to pre-Hispanic symbols, this gigantic sculpture, an homage to neurosurgery, is one of her most recognizable public art pieces.',
+  name: 'Practice questions',
+  question: '1) A networking model is sometimes called what?',
+  answer: 'A networking architecture or networking blueprint.',
   url: 'https://i.imgur.com/Mx7dA2Y.jpg',
   alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
 
 }]
+
+
