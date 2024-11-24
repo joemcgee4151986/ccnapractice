080a938d42b390457645cd9a89d095949654ef09
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-11-24 23:47:10 +0900

    fixed error where no pop up would appear if doing the test

---
+++ main.js
@@ -0,0 +1,39 @@
+let formElement = document.forms['quizForm'];
+formElement.onsubmit = function submitAnswers(){
+	let total = 9;
+	let score = 0;
+	
+	// Get User Input
+	let q1 = document.forms["quizForm"]["q1"].value,
+	    q2 = document.forms["quizForm"]["q2"].value,
+	    q3 = document.forms["quizForm"]["q3"].value,
+	    q4 = document.forms["quizForm"]["q4"].value,
+	    q5 = document.forms["quizForm"]["q5"].value;
+	    q6 = document.forms["quizForm"]["q6"].value;
+ 	    q7 = document.forms["quizForm"]["q7"].value;
+	    q8 = document.forms["quizForm"]["q8"].value;
+	    q9 = document.forms["quizForm"]["q9"].value;
+	// Validation
+	for(i = 1; i <= total;i++){
+		if(eval('q'+i) === null || eval('q'+i) === ''){
+			alert('You missed question '+ i);
+			return false;
+		}
+	}
+	
+	// Set Correct Answers
+	let answers = ["d","b","a","c","b","b","a","d","b"];
+	
+	// Check Answers
+	for(i = 1; i <= total;i++){
+		if(eval('q'+i) === answers[i - 1]){
+			score++;
+		}
+	}
+	
+	// Display Results
+	let results = document.getElementById('results');
+	results.innerHTML = '<h3>You scored <span>'+score+'</span> out of <span>'+total+'</span></h3>';
+	
+	return false;
+}
---
+++ practiceExam/chapterOneQuestionTwo/practiceQuestions.js
@@ -0,0 +1,17 @@
+export const sculptureList = [{
+ /* name: 'Practice questions',
+  question: '1) A networking model is sometimes called what?',*/
+  answer: 'A networking architecture or networking blueprint.',
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
+
+},{
+
+
+  /*name: 'Practice questions',
+  question: '1) A networking model is sometimes called what?',*/
+
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
+
+}]
