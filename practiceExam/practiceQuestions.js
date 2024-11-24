c68a17bacf4690947897e2dcd9affbb80c2dc4d6
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-11-24 12:34:14 +0900

    fixing duplicate text bug

--- practiceExam/practiceQuestions.js
+++ practiceExam/practiceQuestions.js
@@ -8,9 +8,9 @@ export const sculptureList = [{
 },{
 
 
-  name: 'Practice questions',
-  question: '1) A networking model is sometimes called what?',
-  answerTwo: 'A networking architecture or networking blueprint.',
+  /*name: 'Practice questions',
+  question: '1) A networking model is sometimes called what?',*/
+
   url: 'https://i.imgur.com/Mx7dA2Y.jpg',
   alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
 
