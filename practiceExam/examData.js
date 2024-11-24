fbf4558b6273135d951ced555defa732961b5903
joemcgee4151986 <joerobertmcgee@gmail.com>
2024-11-24 12:28:28 +0900

    adding extra pages for questions

--- practiceExam/examData.js
+++ practiceExam/examData.js
@@ -6,8 +6,8 @@ const container = document.getElementById('root');
 // Render the sculpture data dynamically
 const sculpture = sculptureList[0];
 container.innerHTML = `
-  <h1>${sculpture.name}</h1>
-  <h2>${sculpture.question}</h2>
+ 
   <p>${sculpture.answer}</p>
+
   <img src="${sculpture.url}" alt="${sculpture.alt}" />
 `;
--- practiceExam/practiceQuestions.js
+++ practiceExam/practiceQuestions.js
@@ -5,5 +5,13 @@ export const sculptureList = [{
   url: 'https://i.imgur.com/Mx7dA2Y.jpg',
   alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
 
-}]
+},{
 
+
+  name: 'Practice questions',
+  question: '1) A networking model is sometimes called what?',
+  answerTwo: 'A networking architecture or networking blueprint.',
+  url: 'https://i.imgur.com/Mx7dA2Y.jpg',
+  alt: 'A bronze statue of two crossed hands delicately holding a human brain in their fingertips.'
+
+}]
