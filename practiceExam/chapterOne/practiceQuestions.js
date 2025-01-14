b2a4e7938744818f5f4001efc1fd8cfd61acecf9
joemcgee4151986 <joerobertmcgee@gmail.com>
2025-01-14 17:20:55 +0900

    removed unncessary comments

--- practiceExam/chapterOne/practiceQuestions.js
+++ practiceExam/chapterOne/practiceQuestions.js
@@ -1,11 +1,13 @@
-export const sculptureList = [
+export const practiceQuestions = [
   {
     answer: "A networking architecture or networking blueprint.",
     answer2: "Application, Transport, Network, Data-link, Physical.",
     answer3:"Encapsulation refers to putting headers around some data. Basically, it's like wrapping a gift. ",
+    answer4:"",
+    answer5:"",
+    answer6:"",
     url: "https://i.imgur.com/wqijLD4.jpg",
     url2: "https://i.imgur.com/ZuP26kh.jpg",
     url3: "https://i.imgur.com/bZ3E6wY.jpg",
-    alt: "A bronze statue of two crossed hands delicately holding a human brain in their fingertips.",
   },
 ];
--- practiceExam/chapterOne/questionThree/examData.js
+++ practiceExam/chapterOne/questionThree/examData.js
@@ -1,13 +1,13 @@
-import { sculptureList } from "../practiceQuestions.js";
+import { practiceQuestions } from "../practiceQuestions.js";
 
-// Select the container
+
 const container = document.getElementById("root");
 
-// Render the sculpture data dynamically
-const sculpture = sculptureList[0];
+
+const question = practiceQuestions[0];
 container.innerHTML = `
  
-  <p>${sculpture.answer3}</p>
+  <p>${question.answer3}</p>
 
-  <img src="${sculpture.url3}" alt="${sculpture.alt}" /> 
+  <img src="${question.url3}" alt="${question.alt}" /> 
 `;
