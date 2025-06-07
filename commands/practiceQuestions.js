9c9b77bd62f8540c449302baae18ae2b0cc376a7
joemcgee4151986 <joerobertmcgee@gmail.com>
2025-06-07 17:15:35 +0900

    added another folder

--- commands/practiceQuestions.js
+++ commands/practiceQuestions.js
@@ -6,7 +6,8 @@ export const practiceQuestions = [
     url4:"https://i.imgur.com/ouRlAqw.jpeg",
     url5: "https://i.imgur.com/3yakGBd.jpeg",
     url6: "https://i.imgur.com/glv7UCv.jpeg",
-    url7: "https://i.imgur.com/Wu8iI46.jpeg"
+    url7: "https://i.imgur.com/Wu8iI46.jpeg",
+    url8: "blob:https://imgur.com/d9af5fed-26e0-4a7c-a0f8-3f5746a04713"
 
     
   }
