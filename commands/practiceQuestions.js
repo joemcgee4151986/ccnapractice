1825d0dcb59e5fb1bd3774eba1b9ec536f190106
joemcgee4151986 <joerobertmcgee@gmail.com>
2025-06-07 14:58:15 +0900

    fixing answeriosModes practice question

--- commands/practiceQuestions.js
+++ commands/practiceQuestions.js
@@ -2,7 +2,7 @@ export const practiceQuestions = [
   {
     answeruser: "A very basic amount of commands. You can tell you're in user exec mode by the '>' symbol. For example: R1>.",
     answerexec: "Allows you access to more commands. Allows you to access configuration terminal mode. Represented by the '#' symbold. Example: S1#. In this mode, you can access global configuration mode using configuration terminal",
-    answeriosModes:"Access privileged exec mode by typing enable. Disable returns you to user exec mode. The line subconfiguration mode is accessed by typing -- switch(config)# line console 0  switch(config-line)# exit -- switch(config)#",
+    answeriosModes:"Access privileged exec mode by typing enable. Disable returns you to user exec mode. The line subconfiguration mode is accessed by typing || switch(config)# line console 0 || switch(config-line)# exit || switch(config)#",
 
     answer4:"Servers and clients.",
     answer5:"",
